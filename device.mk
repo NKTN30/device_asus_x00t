b12fff9b26b1bb57aee3f8a0bf94abd495240243
nktn30 <nkwhitehat@gmail.com>
2021-08-31 07:40:43 +0530

    X00TD: Build OnePlusLauncher
    * if exists
    
    Signed-off-by: nktn30 <nkwhitehat@gmail.com>

--- device.mk
+++ device.mk
@@ -15,6 +15,9 @@ TARGET_SCREEN_WIDTH := 1080
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# OnePlusLauncher
+$(call inherit-product-if-exists, vendor/oplauncher/OPLauncher.mk)
+
 # Powerhint
 EAS_POWERHINT_VARIANT := sdm636
 
